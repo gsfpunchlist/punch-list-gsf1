975e0301261c5b36859cf3422ff61e56f2525d40
gsfpunchlist <gsfpunchlist@gmail.com>
2026-04-20 12:22:22 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Punch List GSF1 — Service Worker
 // Versión del cache — incrementar al actualizar la app
-const CACHE_NAME = 'punchlist-gsf1-v7';
+const CACHE_NAME = 'punchlist-gsf1-v9';
 const ASSETS = [
   './',
   './index.html',
